23c98fb9ac100028ce3168322a2d0e48adc6f988
Idan Lev <idan.93lev@gmail.com>
2022-07-27 11:11:17 +0300

    implementation of socketIO to server 1

--- server/index.js
+++ server/index.js
@@ -5,13 +5,21 @@ const mongoose = require("mongoose");
 const userRoutes = require("./routes/routes.js");
 
 const uri = process.env.MONGODB_URI;
-const PORT = process.env.PORT || 3001;
-const app = express();
+const PORT = process.env.PORT || 5001;
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(userRoutes);
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET, POST']
+  }
+})
 
 mongoose.connect(uri, () => console.log("connected to mongoDB"));
 

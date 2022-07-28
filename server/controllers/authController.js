d38e20ccbdd8060e3642f62d6782d3f07f62a644
Idan Lev <idan.93lev@gmail.com>
2022-07-28 16:39:41 +0300

    auth register users fixed HEADERS issue

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -20,33 +20,30 @@ const register = async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
-    console.log(err);
+    res.status(500).json(err);
   }
 };
 
 const login = async (req, res) => {
   try {
-    const { username, email, password } = req.body;
+    const { email, password } = req.body;
+    
+    const user = await UserModel.findOne({ email: email });
+    if(!user) {
+      res.status(404).json("user not found");
+      return;
+    }
 
-    // generate new password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    const validPassword = await bcrypt.compare(password, user.password);
+    if(!validPassword) {
+      res.status(400).json("wrong password");
+      return;
+    }
 
-    // create new user
-    const newUser = await new UserModel({
-      username: username,
-      email: email,
-      password: hashedPassword,
-    });
-
-    // save user and respond
-    const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
-    console.log(err);
+    res.status(500).json(err);
   }
 };
-
-
 
 module.exports = { register, login };

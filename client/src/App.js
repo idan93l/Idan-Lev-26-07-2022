5a41098f8183b52caabeed32efd640782689866a
Idan Lev <idan.93lev@gmail.com>
2022-07-29 09:27:51 +0300

    added login and register pages design only

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 import Profile from "./pages/Profile/Profile";
+import Register from "./pages/Register/Register";
 
 function App() {
   return <>
     {/* <Home /> */}
-    <Profile />
+    {/* <Login /> */}
+    <Register />
   </>;
 }
 
